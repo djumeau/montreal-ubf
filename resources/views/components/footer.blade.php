f66bac3a5de9eb10102dd21be3b73bfb82c0b73c
David Jumeau <djumeau@gmail.com>
2026-06-20 14:20:19 -0400

    Footer Links

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,3 +1,32 @@
-<div>
-    <!-- Smile, breathe, and go slowly. - Thich Nhat Hanh -->
-</div>
+<!-- Footer -->
+<footer class="bg-blue-900 text-white py-2">
+
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+
+        <!-- Row-1 - Main Footer Content -->
+        <div class="grid grid-cols-1 md:grid-cols-2 gap-8 mb-8">
+            <!-- Column 1 -->
+            <div class="pl-4">
+                <h3 class="font-bold">Addresse</h3>
+                <p>2627 rue Ryde</p>
+                <p>Montréal, QC, H3K 1R7</p>
+            </div>
+
+            <!-- Column 2 -->
+            <div class="flex flex-col items-left text-left py-0">
+                <h3 class="font-bold">Liens</h3>
+                <div class="py-0"><x-nav-link url="/about" :active="request()->routeIs('about')" icon="angle-right">À propos</x-nav-link>
+                </div>
+                <div class="py-0"><x-nav-link url="/events" :active="request()->routeIs('events')"
+                        icon="angle-right">Événements</x-nav-link></div>
+            </div>
+        </div>
+
+        <!-- Bottom Copyright Row -->
+        <div class="text-center pt-4">
+            <p class="text-center p-0">&copy; {{ now()->year }} CBU Montréal UBF</p>
+        </div>
+
+    </div>
+
+</footer>
