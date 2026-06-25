bc5b5064387195f9fb581a388fe4d1f4d17fb57f
David Jumeau <djumeau@gmail.com>
2026-06-25 11:56:42 -0400

    Modified home page.

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,15 +1,27 @@
 <x-layout>
 
-    <div class='w-full flex justify-center mx-auto px-4 pt-2 pb-6'>
+    <div class='w-full flex justify-center px-4 pt-2 pb-6'>
 
         <!-- Responsive Grid Wrapper -->
 
-        <div class='flex flex-col md:flex-row flex-wrap justify-between items-center gap-y-6 max-w-6xl w-full'>
+        <div class='flex flex-col md:flex-row md:items-start flex-wrap justify-center items-center gap-6'>
 
             <x-card image='./images/ministry/montreal_ministry_20190616-original.jpg' title='Nous sommes...'
-                alt='Montreal Ministry 2019' class="md:w-[33%]">
+                alt='Montreal 2019' class="w-full max-w-sml md:w-[30%] md:max-w-none">
                 ... une communauté chrietienne composant d'étudiants et familles dédié à l'étude biblique
                 en mettant l'accent sur l'évagelisation des campus du monde.
+            </x-card>
+
+            <x-card image='./images/other/classroom-circle-card.jpg' title='Ministères' alt="Groupe d'étude"
+                class="w-full max-w-sml md:w-[30%] md:max-w-none">
+                Nos études bibliques est parvenu en groupe et en personne. On préfère de se réunir à notre église mais
+                on offre aussi les études en ligne.
+            </x-card>
+
+            <x-card image='./images/other/earth-from-space-card.jpg' title='Pour le monde' alt='Monde'
+                class="w-full max-w-sml md:w-[30%] md:max-w-none">
+                On participe aussi dans les conférences locales, nationales et internationales. On collabore avec nos
+                ministères dans plus de 90 pays.
             </x-card>
 
         </div>
