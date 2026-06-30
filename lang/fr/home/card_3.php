dfb1590fd3319a0905c4f8eccfd6e954d463f83c
David Jumeau <djumeau@gmail.com>
2026-06-30 15:05:10 -0400

    Modified localization for Card 3 on home page and switched locale to fr

--- lang/fr/home/card_3.php
+++ lang/fr/home/card_3.php
@@ -3,5 +3,5 @@
 return [
     "title" => "Pour le monde",
     "alt" => "Le monde",
-    "blurb_1" => "La CBU est présent dans 96 pays au tour du monde. Nous participons à des confèrences à l'échelle locale, nationale et internationale."
+    "blurb_1" => "La CBU est présent dans 96 pays dans le monde. Nous participons à des confèrences à l'échelle locale, nationale et internationale."
 ];
