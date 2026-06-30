14fd9ecf4bfa3aeb9a74d47746a261ed80d1d48d
David Jumeau <djumeau@gmail.com>
2026-06-30 14:49:15 -0400

    Updated localization of about page.

--- lang/en/about/index.php
+++ lang/en/about/index.php
@@ -2,6 +2,7 @@
 
 return [
     "title" => "About Us",
+    
     "history" => "History",
     "content_1" => "University Bible Fellowship (UBF) was established in South Korea in 1961 under Dr Samuel Lee (a presbyterian pastor) et Sarah Barry (an American missionary).",
     "alt_1" => "Dr. Samuel Lee et Sarah Barry",
@@ -9,5 +10,28 @@ return [
     "alt_2" => "Bible reading class",
     "content_3" => "The ministry spread to various cities in the country and began a missionary movement (Mt 28:19-20) in the 1970s – first in Germany, then the USA, latin america, and eventually the whole world. The minsitry evolved from a para-church ministry into its own denomination. At present (2026), UBF has ministries in 96 countries.",
     "alt_3" => "First CIS conference. Moscow, 1993",
+    "content_4" => "Canadian missions began in 1982 in Winnipeg, Manitoba and has since spread out throughout the country",
+    "alt_4" => "Prayer Circle",
+    "content_5" => "Founded in 1991, the Montreal ministry is composed of professionals, families and young adults.",
+    "alt_5" => "Presentation",
 
+    "mission_vision" => "Mission and Vision",
+    "mv_blurb_1" => "Our mission is to bring the gospel to campus students as it offers salvation to all who believe. (Ro 1:16) We believe that students are the future leaders of society and can be a good influence as well as to share the good news to their peers, to their country, and for the whole world.",
+    "mv_blurb_2" => "For more information about the ministry, visit:",
+    "mv_blurb_3" => ".",
+
+    "statement_faith" => "Statement of Faith",
+    "sf_blurb_1" => "We believe:",
+    "sf_statement_1" => "There is one God in three Persons: God the Father, God the Son, and God the Holy Spirit.",
+    "sf_statement_2" => "In God who created the heavens and the earth and all other things in the universe: that He is the Sovereign Ruler of all things; that the Sovereign God reveals Himself; we believe in his redemptive work and in his final judgment.",
+    "sf_statement_3" => "The Bible is inspired by God; that it is the truth; that it is the final authority in faith and practice.",
+    "sf_statement_4" => "Since the fall of Adam, all people have been under the bondage and power of sin and are deserving of the judgment and wrath of God.",
+    "sf_statement_5" => "In Jesus Christ, who is God and man, through his atoning, sacrificial death on the cross for our sins and his resurrection, is the only way of salvation (Jn 14:6); he alone saves us from sin and judgment and purifies us from the contamination of the world caused by sin.",
+    "sf_statement_6" => "That Jesus Christ rose from the dead, ascended into heaven and sits at the right hand of God the Father.",
+    "sf_statement_7" => "That regeneration is by the work of the Holy Spirit, and that it is necessary if one is to enter the kingdom of God.",
+    "sf_statement_8" => "That God sent his Holy Spirit to empower his church to witness to Jesus to the ends of the earth.",
+    "sf_statement_9" => "That we are made righteous by grace alone, through faith alone. (Ro 1:17)",
+    "sf_statement_10" => "That the Holy Spirit works in the heart of every believer to lead him.",
+    "sf_statement_11" => "That the church is the body of Christ and that all Christians are members of it.",
+    "sf_statement_12" => "That Jesus will come again in glory to judge the living and the dead.",
 ];
--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -21,50 +21,52 @@
 
     <x-text-image :toggleLeft="false" 
     img="./images/history/cdn-campus-mission-1982.jpeg"
-    alt="Cercle de prière">Le ministère canadien a vu le jour en 1982 à Winnipeg, au Manitoba, et s&apos;est répandu à plusieurs villes à travers le pays.</x-text-image>
+    alt="{{__('about/index.alt_4')}}">{{__('about/index.content_4')}}</x-text-image>
 
     <x-text-image :toggleLeft="true" 
     img="./images/history/20240825-presentation.jpg"
-    alt="Présentation à Montreal">Le ministère de Montréal était établit en 1991. L&apos;église est composée de professionnels, de familles et de jeunes adultes.</x-text-image>
+    alt="{{__('about/index.alt_5')}}">{{__('about/index.content_5')}}</x-text-image>
 
     <br/>
 
-    <x-blurb title='Mission et vision' :variant="['slate-900', '#1e3a8a']">
-        <p>Notre mission principale est d&apos;apporter l&apos;Évangile aux étudiants des campus car l&apos;Évanigile offre le salut à tous le monde pour ceux qui croit (Ro 1.16). Également, nous croyons que nos étudiants sont nos futurs leaders. Une fois qu&apos;ils auront mis leur foi en Jésus-Christ comme Seigneur et Sauveur, ils pourront exercer une influence dans tous les aspects de la vie et transmettre la bonne nouvelle de l&apos;Évangile à leurs pairs, à la nation et au monde entier.</p>
+    <x-blurb title="{{__('about/index.mission_vision')}}" :variant="['#1e3a8a', 'slate-900']">
+        <p>{{__('about/index.mv_blurb_1')}}</p>
         
-        <p>Pour plus d&apos;information sur le ministère, visitez <a href='https://ubf.org/about/origin' class='underline' target='_blank'>ubf.org</a>. (En anglais.)</p>
+        <p>{{__('about/index.mv_blurb_2')}} <a href='https://ubf.org/about/origin' class='underline' target='_blank'>ubf.org</a>{{__('about/index.mv_blurb_3')}}</p>
     </x-blurb>
 
     <br/><br/>
 
-    <x-blurb title='Déclaration de foi' :variant="['slate-900', '#1e3a8a']"></x-blurb>
+    <x-blurb title="{{__('about/index.statement_faith')}}" :variant="['slate-900', '#1e3a8a']"></x-blurb>
 
     <br/>
 
-    <h3 class='flex justify-left text-left font-bold text-xl'>Nous croyons :</h3>
+    <h3 class='flex justify-left text-left font-bold text-xl'>{{__('about/index.sf_blurb_1')}}</h3>
 
-    <ul class='list-disc list-outside p-5 space-y-4'>
-        <li>Qu&apos;il existe un seul Dieu en trois Personnes : Dieu le Père, Dieu le Fils et Dieu le Saint-Esprit.</li>
+    <ul class='list-disc list-outside p-5 space-y-4 md:px-12'>
+        <li>{{__('about/index.sf_statement_1')}}</li>
 
-        <li>Dieu a créé les cieux, la terre et toutes les autres choses de l&apos;univers; qu&apos;Il est le Souverain de toutes choses; que ce Dieu souverain se révèle; nous croyons en son œuvre rédemptrice et en son dernier jugement.</li>
+        <li>{{__('about/index.sf_statement_2')}}</li>
 
-        <li>La Bible est inspirée par Dieu; elle est la vérité; elle est l&apos;autorité suprême en matière de foi et de pratique.</li>
+        <li>{{__('about/index.sf_statement_3')}}</li>
 
-        <li>Depuis la chute d&apos;Adam, toute l'humanité est sous l’esclavage et la puissance du péché et méritent le jugement et la colère de Dieu.</li>
+        <li>{{__('about/index.sf_statement_4')}}</li>
 
-        <li>Jésus-Christ, qui est à la fois Dieu et homme, par sa mort expiatoire et sacrificielle sur la croix pour nos péchés et par sa résurrection, est le seul chemin du salut; (Jn 14.6) lui seul nous sauve du péché et du jugement et nous purifie de la souillure du monde causée par le péché.</li>
+        <li>{{__('about/index.sf_statement_5')}}</li>
 
-        <li>Jésus-Christ est ressuscité des morts, est monté au ciel et se siège à la droite de Dieu le Père.</li>
+        <li>{{__('about/index.sf_statement_6')}}</li>
 
-        <li>La régénération est l&apos;œuvre du Saint-Esprit, et elle est nécessaire pour entrer dans le royaume de Dieu. Nous croyons que Dieu a envoyé son Saint-Esprit pour donner à son Église la puissance de témoigner de Jésus jusqu&apos;aux extrémités de la terre.</li>
+        <li>{{__('about/index.sf_statement_7')}}</li>
 
-        <li>Nous sommes justifiés par la grâce seule, par la foi seule.</li>
+        <li>{{__('about/index.sf_statement_8')}}</li>
 
-        <li>Le Saint-Esprit agit dans le cœur de chaque croyant pour le guider.</li>
+        <li>{{__('about/index.sf_statement_9')}}</li>
 
-        <li>L&apos;Église universelle est le corps du Christ et tous les chrétiens en sont membres.</li>
+        <li>{{__('about/index.sf_statement_10')}}</li>
 
-        <li>Jésus reviendra dans la gloire pour juger les vivants et les morts.</li>
+        <li>{{__('about/index.sf_statement_11')}}</li>
+
+        <li>{{__('about/index.sf_statement_12')}}</li>
 
     </ul>
 
