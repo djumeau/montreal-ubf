d9c598c1698ff6f91224c67d1eb0cfa19f92322f
David Jumeau <djumeau@gmail.com>
2026-06-30 16:45:54 -0400

    Changes to web.php route.

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AboutController;
 use App\Http\Controllers\HomeController;
-use App\Http\Controllers\BibleController;
-use App\Http\Controllers\ArticleController;
 use App\Http\Controllers\EventController;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('/about', [AboutController::class, 'index'])->name('about');
 Route::get('/events', [EventController::class, 'index'])->name('events');
-Route::resource('articles', ArticleController::class);
