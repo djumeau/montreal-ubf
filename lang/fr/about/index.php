560cb453b20d2d47d135b7558405769e4cc77bfe
David Jumeau <djumeau@gmail.com>
2026-06-30 15:32:42 -0400

    Localization in About page. Adjusted Associative array.

--- lang/fr/about/index.php
+++ lang/fr/about/index.php
@@ -3,36 +3,49 @@
 return [
     "title" => "À propos de nous",
 
-    "history" => "Historique",
-    "content_1" => "La communion biblique universitaire (CBU), donc 'University Bible Fellowship (UBF)' en anglais, a pris son début en Corée du Sud en 1961 sous Dr Samuel Lee (un pasteur presbytérien) et Sarah Barry (missionnaire américaine).",
-    "alt_1" => "Dr. Samuel Lee et Sarah Barry",
-    "content_2" => "À l'origine, le ministère s'adressaient aux étudiants sud-coréens par un club de lecture biblique.",
-    "alt_2" => "Lecture biblique en classe.",
-    "content_3" => "Le ministère s'est étendu à diverses villes du pays et, à partir des années 1970, le ministère envoya des missionaires en Allemagne, en Amérique, en Amérique latine, et puis, au monde entier. L'église s'est passée du statut para-ecclésiastique à celui d'un dénomination. Elle compte actuellement des ministères dans 96 pays.",
-    "alt_3" => "1993 conférence en Russie",
-    "content_4" => "Le ministère canadien a vu le jour en 1982 à Winnipeg, au Manitoba, et s'est répandu à plusieurs villes à travers le pays.",
-    "alt_4" => "Cercle de prière",
-    "content_5" => "Le ministère de Montréal était établit en 1991. L'église est composée de professionnels, de familles et de jeunes adultes.",
-    "alt_5" => "Présentation",
+    "history" => [
 
-    "mission_vision" => "Mission et vision",
-    "mv_blurb_1" => "Notre mission principale est d'apporter l'Évangile aux étudiants des campus car elle offre le salut à tous qui croient (Ro 1.16). Également, nous croyons que nos étudiants sont nos futurs leaders. Ils peuvent exercer une bonne influence et de transmettre la bonne nouvelle à leurs pairs, à la nation et au monde entier.",
-    "mv_blurb_2" => "Pour plus d'information sur le ministère, visitez",
-    "mv_blurb_3" => ". (En anglais.)",
+        "title" => "Historique",
+        "content_1" => "La communion biblique universitaire (CBU), donc 'University Bible Fellowship (UBF)' en anglais, a pris son début en Corée du Sud en 1961 sous Dr Samuel Lee (un pasteur presbytérien) et Sarah Barry (missionnaire américaine).",
+        "alt_1" => "Dr. Samuel Lee et Sarah Barry",
+        "content_2" => "À l'origine, le ministère s'adressaient aux étudiants sud-coréens par un club de lecture biblique.",
+        "alt_2" => "Lecture biblique en classe.",
+        "content_3" => "Le ministère s'est étendu à diverses villes du pays et, à partir des années 1970, le ministère envoya des missionaires en Allemagne, en Amérique, en Amérique latine, et puis, au monde entier. L'église s'est passée du statut para-ecclésiastique à celui d'un dénomination. Elle compte actuellement des ministères dans 96 pays.",
+        "alt_3" => "1993 conférence en Russie",
+        "content_4" => "Le ministère canadien a vu le jour en 1982 à Winnipeg, au Manitoba, et s'est répandu à plusieurs villes à travers le pays.",
+        "alt_4" => "Cercle de prière",
+        "content_5" => "Le ministère de Montréal était établit en 1991. L'église est composée de professionnels, de familles et de jeunes adultes.",
+        "alt_5" => "Présentation",
 
-    "statement_faith" => "Déclaration de foi",
-    "sf_blurb_1" => "Nous croyons:",
-    "sf_statement_1" => "Qu'il existe un seul Dieu en trois Personnes : Dieu le Père, Dieu le Fils et Dieu le Saint-Esprit.",
-    "sf_statement_2" => "Dieu a créé les cieux, la terre et toutes les autres choses de l'univers; qu'Il est le Souverain de toutes choses; que ce Dieu souverain se révèle; nous croyons en son œuvre rédemptrice et en son dernier jugement.",
-    "sf_statement_3" => "La Bible est inspirée par Dieu; elle est la vérité; elle est l'autorité suprême en matière de foi et de pratique.",
-    "sf_statement_4" => "Depuis la chute d'Adam, toute l'humanité est sous l’esclavage et la puissance du péché et méritent le jugement et la colère de Dieu.",
-    "sf_statement_5" => "Jésus-Christ, qui est à la fois Dieu et homme, par sa mort expiatoire et sacrificielle sur la croix pour nos péchés et par sa résurrection, est le seul chemin du salut; (Jn 14.6) lui seul nous sauve du péché et du jugement et nous purifie de la souillure du monde causée par le péché.",
-    "sf_statement_6" => "Jésus-Christ est ressuscité des morts, est monté au ciel et se siège à la droite de Dieu le Père.",
-    "sf_statement_7" => "La régénération est l'œuvre du Saint-Esprit, et elle est nécessaire pour entrer dans le royaume de Dieu. (Jn 3.3)",
-    "sf_statement_8" => "Nous croyons que Dieu a envoyé son Saint-Esprit pour donner à son Église la puissance de témoigner de Jésus jusqu'aux extrémités de la terre. (Ac 1.8)",
-    "sf_statement_9" => "Nous sommes justifiés par la grâce seule, par la foi seule. (Ro 1.17)",
-    "sf_statement_10" => "Le Saint-Esprit agit dans le cœur de chaque croyant pour le guider.",
-    "sf_statement_11" => "L'Église universelle est le corps du Christ et tous les chrétiens en sont membres.",
-    "sf_statement_12" => "Jésus reviendra dans la gloire pour juger les vivants et les morts.",
+    ], 
 
+    "mission_vision" => [
+
+        "title" => "Mission et vision",
+        "mv_blurb_1" => "Notre mission principale est d'apporter l'Évangile aux étudiants des campus car elle offre le salut à tous qui croient (Ro 1.16). Également, nous croyons que nos étudiants sont nos futurs leaders. Ils peuvent exercer une bonne influence et de transmettre la bonne nouvelle à leurs pairs, à la nation et au monde entier.",
+        "mv_blurb_2" => "Pour plus d'information sur le ministère, visitez",
+        "mv_blurb_3" => ". (En anglais.)",
+
+    ],
+    
+    "statement_faith" => [
+
+        "title" => "Déclaration de foi",
+        "sf_blurb_1" => "Nous croyons:",
+        "sf_statement_1" => "Qu'il existe un seul Dieu en trois Personnes : Dieu le Père, Dieu le Fils et Dieu le Saint-Esprit.",
+        "sf_statement_2" => "Dieu a créé les cieux, la terre et toutes les autres choses de l'univers; qu'Il est le Souverain de toutes choses; que ce Dieu souverain se révèle; nous croyons en son œuvre rédemptrice et en son dernier jugement.",
+        "sf_statement_3" => "La Bible est inspirée par Dieu; elle est la vérité; elle est l'autorité suprême en matière de foi et de pratique.",
+        "sf_statement_4" => "Depuis la chute d'Adam, toute l'humanité est sous l’esclavage et la puissance du péché et méritent le jugement et la colère de Dieu.",
+        "sf_statement_5" => "Jésus-Christ, qui est à la fois Dieu et homme, par sa mort expiatoire et sacrificielle sur la croix pour nos péchés et par sa résurrection, est le seul chemin du salut; (Jn 14.6) lui seul nous sauve du péché et du jugement et nous purifie de la souillure du monde causée par le péché.",
+        "sf_statement_6" => "Jésus-Christ est ressuscité des morts, est monté au ciel et se siège à la droite de Dieu le Père.",
+        "sf_statement_7" => "La régénération est l'œuvre du Saint-Esprit, et elle est nécessaire pour entrer dans le royaume de Dieu. (Jn 3.3)",
+        "sf_statement_8" => "Nous croyons que Dieu a envoyé son Saint-Esprit pour donner à son Église la puissance de témoigner de Jésus jusqu'aux extrémités de la terre. (Ac 1.8)",
+        "sf_statement_9" => "Nous sommes justifiés par la grâce seule, par la foi seule. (Ro 1.17)",
+        "sf_statement_10" => "Le Saint-Esprit agit dans le cœur de chaque croyant pour le guider.",
+        "sf_statement_11" => "L'Église universelle est le corps du Christ et tous les chrétiens en sont membres.",
+        "sf_statement_12" => "Jésus reviendra dans la gloire pour juger les vivants et les morts.",
+
+    ],
+    
+    
 ];
--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -4,69 +4,69 @@
 
     <h1 class='text-right text-4xl font-bold pb-8'>{{__('about/index.title')}}</h1>
 
-    <x-blurb title="{{__('about/index.history')}}" :variant="['slate-900', '#1e3a8a']"></x-blurb>
+    <x-blurb title="{{__('about/index.history.title')}}" :variant="['slate-900', '#1e3a8a']"></x-blurb>
 
     <x-text-image :toggleLeft="true" 
     img="./images/history/lee-barry.jpg"
-    alt="{{__('about/index.alt_1')}}">{{__('about/index.content_1')}}</x-text-image>
+    alt="{{__('about/index.alt_1')}}">{{__('about/index.history.content_1')}}</x-text-image>
 
     <x-text-image :toggleLeft="false" 
     img="./images/history/bible-reading-class-ubf-korea.jpg"
-    alt="{{__('about/index.alt_2')}}">{{__('about/index.content_2')}}</x-text-image>
+    alt="{{__('about/index.alt_2')}}">{{__('about/index.history.content_2')}}</x-text-image>
 
     <x-text-image :toggleLeft="true" 
     img="./images/history/first-cis-conference.jpg"
-    alt="{{__('about/index.alt_3')}}">{{__('about/index.content_3')}}</x-text-image>
+    alt="{{__('about/index.alt_3')}}">{{__('about/index.history.content_3')}}</x-text-image>
 
 
     <x-text-image :toggleLeft="false" 
     img="./images/history/cdn-campus-mission-1982.jpeg"
-    alt="{{__('about/index.alt_4')}}">{{__('about/index.content_4')}}</x-text-image>
+    alt="{{__('about/index.alt_4')}}">{{__('about/index.history.content_4')}}</x-text-image>
 
     <x-text-image :toggleLeft="true" 
     img="./images/history/20240825-presentation.jpg"
-    alt="{{__('about/index.alt_5')}}">{{__('about/index.content_5')}}</x-text-image>
+    alt="{{__('about/index.alt_5')}}">{{__('about/index.history.content_5')}}</x-text-image>
 
     <br/>
 
-    <x-blurb title="{{__('about/index.mission_vision')}}" :variant="['#1e3a8a', 'slate-900']">
-        <p>{{__('about/index.mv_blurb_1')}}</p>
+    <x-blurb title="{{__('about/index.mission_vision.title')}}" :variant="['#1e3a8a', 'slate-900']">
+        <p>{{__('about/index.mission_vision.mv_blurb_1')}}</p>
         
-        <p>{{__('about/index.mv_blurb_2')}} <a href='https://ubf.org/about/origin' class='underline' target='_blank'>ubf.org</a>{{__('about/index.mv_blurb_3')}}</p>
+        <p>{{__('about/index.mission_vision.mv_blurb_2')}} <a href='https://ubf.org/about/origin' class='underline' target='_blank'>ubf.org</a>{{__('about/index.mission_vision.mv_blurb_3')}}</p>
     </x-blurb>
 
     <br/><br/>
 
-    <x-blurb title="{{__('about/index.statement_faith')}}" :variant="['slate-900', '#1e3a8a']"></x-blurb>
+    <x-blurb title="{{__('about/index.statement_faith.title')}}" :variant="['slate-900', '#1e3a8a']"></x-blurb>
 
     <br/>
 
-    <h3 class='flex justify-left text-left font-bold text-xl'>{{__('about/index.sf_blurb_1')}}</h3>
+    <h3 class='flex justify-left text-left font-bold text-xl'>{{__('about/index.statement_faith.sf_blurb_1')}}</h3>
 
     <ul class='list-disc list-outside p-5 space-y-4 md:px-12'>
-        <li>{{__('about/index.sf_statement_1')}}</li>
+        <li>{{__('about/index.statement_faith.sf_statement_1')}}</li>
 
-        <li>{{__('about/index.sf_statement_2')}}</li>
+        <li>{{__('about/index.statement_faith.sf_statement_2')}}</li>
 
-        <li>{{__('about/index.sf_statement_3')}}</li>
+        <li>{{__('about/index.statement_faith.sf_statement_3')}}</li>
 
-        <li>{{__('about/index.sf_statement_4')}}</li>
+        <li>{{__('about/index.statement_faith.sf_statement_4')}}</li>
 
-        <li>{{__('about/index.sf_statement_5')}}</li>
+        <li>{{__('about/index.statement_faith.sf_statement_5')}}</li>
 
-        <li>{{__('about/index.sf_statement_6')}}</li>
+        <li>{{__('about/index.statement_faith.sf_statement_6')}}</li>
 
-        <li>{{__('about/index.sf_statement_7')}}</li>
+        <li>{{__('about/index.statement_faith.sf_statement_7')}}</li>
 
-        <li>{{__('about/index.sf_statement_8')}}</li>
+        <li>{{__('about/index.statement_faith.sf_statement_8')}}</li>
 
-        <li>{{__('about/index.sf_statement_9')}}</li>
+        <li>{{__('about/index.statement_faith.sf_statement_9')}}</li>
 
-        <li>{{__('about/index.sf_statement_10')}}</li>
+        <li>{{__('about/index.statement_faith.sf_statement_10')}}</li>
 
-        <li>{{__('about/index.sf_statement_11')}}</li>
+        <li>{{__('about/index.statement_faith.sf_statement_11')}}</li>
 
-        <li>{{__('about/index.sf_statement_12')}}</li>
+        <li>{{__('about/index.statement_faith.sf_statement_12')}}</li>
 
     </ul>
 
