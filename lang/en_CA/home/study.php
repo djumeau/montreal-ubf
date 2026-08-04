3d264ffa850e33e538ce9e1bcdcd428200111edb
David Jumeau <djumeau@gmail.com>
2026-08-03 21:32:06 -0400

    Added Jn 8:31-32

--- lang/en_CA/home/study.php
+++ lang/en_CA/home/study.php
@@ -4,13 +4,13 @@ return [
 
     "heading" => "Bible Study Series",
     "book" => "John’s Gospel",
-    "title" => "Jesus is the Light of the World",
+    "title" => "The Truth Will Set You Free",
     "dateStamp" => "August 2nd, 2026",
-    "biblePassage" => "John 8.12-30",
-    "bibleLink" => "https://www.biblegateway.com/passage/?search=John%208%3A12-30&version=NIV",
+    "biblePassage" => "John 8.31-59",
+    "bibleLink" => "https://www.biblegateway.com/passage/?search=John%208%3A31-59&version=NIV",
     "questionSheet" => "Questionnaire",
     "dir" => "nt/john_2026",
-    "pdf" => "jn_08.12-30.q.pdf",
-    "docx" => "jn_08.12-30.q.docx",
+    "pdf" => "jn_08.31-59.q.pdf",
+    "docx" => "jn_08.31-59.q.docx",
 
 ];
--- lang/fr_CA/home/study.php
+++ lang/fr_CA/home/study.php
@@ -4,13 +4,13 @@ return [
 
     "heading" => "Série d’étude biblique",
     "book" => "L’évangile de Jean",
-    "title" => "Jésus est la lumière du monde",
-    "dateStamp" => "Le 2 août, 2026",
-    "biblePassage" => "Jean 8.12-30",
-    "bibleLink" => "https://www.biblegateway.com/passage/?search=Jean%208%3A12-30&version=SG21",
+    "title" => "La vérité vous rendra libres",
+    "dateStamp" => "Le 9 août, 2026",
+    "biblePassage" => "Jean 8.31-59",
+    "bibleLink" => "https://www.biblegateway.com/passage/?search=Jean%208%3A31-59&version=SG21",
     "questionSheet" => "Questionnaire",
     "dir" => "nt/john_2026",
-    "pdf" => "jn_08.12-30.q.fr.pdf",
-    "docx" => "jn_08.12-30.q.fr.docx",
+    "pdf" => "jn_08.31-59.q.fr.pdf",
+    "docx" => "jn_08.31-59.q.fr.docx",
 
 ];
