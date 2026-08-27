b4c283acc7a15046a42782dcbafdc192e1db1f39
David Jumeau <djumeau@gmail.com>
2026-08-27 13:34:09 -0400

    Update giving

--- lang/en_CA/giving.php
+++ lang/en_CA/giving.php
@@ -7,7 +7,7 @@ return [
 
     "introduction" => "Thank you for your interest in giving to Montreal UBF. As we are encouraged to freely give as Jesus has commanded. Any monetary  support is appreciated to advance the work of God in Montreal, Quebec, Canada and for the whole world. These resources cover expenses to maintain our church building, and to support world campus mission. There are several ways to give:",
 
-    "inperson" => ["title" => "In person", "instructions" => "During our offering time during Sunday worship."],
+    "inperson" => ["title" => "In Person", "instructions" => "During our offering time during Sunday worship."],
 
     "online" => ["title" => "Online", "instructions" => "We have created a Zeffy account to provide access to online giving. Either scan the QR code on the screen with your phone or select the button below to access the site.", "note" => "Note: You are not obligated to pay a service fee. Please read the instructions from the video to learn how to avoid doing so."],
 
