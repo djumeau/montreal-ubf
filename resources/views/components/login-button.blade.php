e30af3775feb5804c1af4643657bacae9b4bf3e7
David Jumeau <djumeau@gmail.com>
2026-09-04 09:29:21 -0400

    Fixed loginbutton

--- resources/views/components/login-button.blade.php
+++ resources/views/components/login-button.blade.php
@@ -1,4 +1,9 @@
-<a href="{{ route(__('nav.login.name')) }}">
+@props([
+    'icon' => null,
+    'isMobile' => false,
+])
+
+<a href="{{ route(__('nav.login.name')) }}"
     class="bg-sky-900 justify-right hover:bg-sky-950 text-white font-bold py-2 px-4 rounded
     outline-1 outline-white hover:outline-2 focus:shadow-outline">
     <i class="fa fa-user p-0 mr-1"></i>
