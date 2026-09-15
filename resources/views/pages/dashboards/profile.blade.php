cf7b9508181a6a971e31f6821c5803410745bb4f
David Jumeau <djumeau@gmail.com>
2026-09-15 12:15:59 -0400

    Modified Profile for mobile.

--- resources/views/pages/dashboards/profile.blade.php
+++ resources/views/pages/dashboards/profile.blade.php
@@ -126,16 +126,16 @@
 
                     <div class="flex flex-col mr-4 items-center justify-center gap-y-2 p-2 border rounded-sm outline-white mb-8">
 
-                        <x-inputs.text class="w-58 md:w-100" id="name" name="name"
+                        <x-inputs.text class="w-50 md:w-100" id="name" name="name"
                             value="{{ auth()->user()->name }}" />
 
-                        <x-inputs.text class="w-58 md:w-100" id="email" name="email" type="email"
+                        <x-inputs.text class="w-50 md:w-100" id="email" name="email" type="email"
                             value="{{ auth()->user()->email }}" />
 
-                        <x-inputs.text class="w-58 md:w-100" id="password" name="password" type="password"
+                        <x-inputs.text class="w-50 md:w-100" id="password" name="password" type="password"
                             placeholder="{{ __('auth/index.password') }}" value="{{ old('password') }}" />
 
-                        <x-inputs.text class="w-58 md:w-100" id="password_confirmation" name="password_confirmation"
+                        <x-inputs.text class="w-50 md:w-100" id="password_confirmation" name="password_confirmation"
                             type="password" placeholder="{{ __('auth/index.confirm_password') }}"
                             value="{{ old('password_confirmation') }}" />
 
