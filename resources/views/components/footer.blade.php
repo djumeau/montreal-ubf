dbbb794267d6b4be23ffc9fc76b1133b4c25cff5
David Jumeau <djumeau@gmail.com>
2026-09-17 12:26:14 -0400

    Modified footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -13,13 +13,13 @@
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
 
         <!-- Row-1 - Main Footer Content -->
-        <div class="grid grid-cols-1 md:grid-cols-2 gap-8 mb-8">
+        <div class="grid grid-cols-1 md:grid-cols-2 gap-8 mb-4">
 
             <!-- Column 1 -->
             <div class="pl-4">
 
                 <!--Address Container -->
-                <div class="address-container" style="display: flex; align-items: center; gap: 15px;">
+                <div class="address-container" style="display: flex; align-items: center; gap: 12px;">
 
                     <!-- Left Side: Clickable Thumbnail Image -->
                     <a href="https://www.google.com/maps/place/University+Bible+Fellowship+Missionary+Church/@45.4755238,-73.5676033,17z/data=!3m1!4b1!4m6!3m5!1s0x4cc9107f9e01be35:0x3487a6c7fa11c19!8m2!3d45.4755201!4d-73.5650284!16s%2Fg%2F1tz75ldw?entry=ttu&g_ep=EgoyMDI2MDgyNC4wIKXMDSoASAFQAw%3D%3D" target="_blank" rel="noopener noreferrer" style="flex-shrink: 0;">
@@ -81,7 +81,7 @@
         </div>
 
         <!-- Bottom Copyright Row -->
-        <div class="text-center p-8">
+        <div class="text-center mb-16">
             <p class="text-center p-0">&copy; {{ now()->year }} {{__('footer.name')}}</p>
         </div>
 
